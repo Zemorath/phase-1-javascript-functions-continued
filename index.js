95e6cd7f3a6409305751c686ac80c5f7536eae47
Zemorath <trentward10@gmail.com>
2023-05-01 19:24:39 -0500

    Functions Continued Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+saturdayFun();
+saturdayFun("watch a movie!");
+
+function mondayWork (activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+mondayWork();
+
+
+function wrapAdjective(visualFlair="*") {
+    const describeMe = function(description="special") {
+        return "You are " + visualFlair + description + visualFlair + "!";
+    };
+    return describeMe;
+}
